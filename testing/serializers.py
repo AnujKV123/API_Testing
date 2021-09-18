45f6d7b072605fabef451ebc0de0755965304ec6
Anuj@123 <65813580+AnujKV123@users.noreply.github.com>
2021-09-18 11:50:33 +0530

    commit

--- testing/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import OTPcontact
-
-
-class verify_serializers(serializers.ModelSerializer):
-    class Meta:
-        model = OTPcontact
-        fields = ['Contact', 'OTP','Name', 'Email']
-
